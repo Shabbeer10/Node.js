1066e853ee7fe066f78dc3d913176ade491cc46c
Shabbeer Petersen <shabbeer.petersen@capaciti.org.za>
2024-10-11 15:34:58 +0200

    cleanup

--- testAPI.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-
-const port = 3000;
-const data = 'items.json';
-
-const server = http.createServer((req,res) =>{})
